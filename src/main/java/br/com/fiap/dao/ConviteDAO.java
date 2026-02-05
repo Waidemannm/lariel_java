8b92ff074a76bc2cc420e5498dfa9ba9c9ad59b0
Waidemannm <mjrmolinillo@icloud.com>
2026-02-05 16:02:23 -0300

    arrumando delete convite

--- src/main/java/br/com/fiap/dao/ConviteDAO.java
+++ src/main/java/br/com/fiap/dao/ConviteDAO.java
@@ -69,6 +69,7 @@ public class ConviteDAO {
         String sql = "DELETE FROM T_LS_CONVIDADOS WHERE ID_NM_CONVITE = ?; DELETE FROM T_LS_NM_CONVITE WHERE ID_NM_CONVITE = ?";
         try(PreparedStatement ps = ConnectionFactory.getConnection().prepareStatement(sql)){
             ps.setString(1, idConvite);
+            ps.setString(2, idConvite);
             return ps.executeUpdate() > 0;
         }catch (SQLException e ){
             System.out.println("Erro ao deletar convite: " + e.getMessage());
