cce5cd684940282bf8cbf1937038be92c7035290
Waidemannm <mjrmolinillo@icloud.com>
2026-02-19 15:30:24 -0300

    arrumando  metodo updateStatus

--- src/main/java/br/com/fiap/dao/ConvidadoDAO.java
+++ src/main/java/br/com/fiap/dao/ConvidadoDAO.java
@@ -102,7 +102,7 @@ public class ConvidadoDAO {
     }
 
     public ConvidadoTO updateStatus(ConvidadoTO convidadoTO){
-        String sql = "UPDATE T_LS_CONVIDADOS SET ST_PRESENCA = C WHERE ID_CONVIDADO = ?";
+        String sql = "UPDATE T_LS_CONVIDADOS SET ST_PRESENCA = 'C' WHERE ID_CONVIDADO = ?";
         try(PreparedStatement ps = ConnectionFactory.getConnection().prepareStatement(sql)){
             ps.setLong(1, convidadoTO.getIdConvidado());
             if (ps.executeUpdate() > 0) {
